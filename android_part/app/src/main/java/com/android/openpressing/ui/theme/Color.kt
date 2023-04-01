89236f46b726ceb70fe4cd5c4dd652ed1e593d37
KengniJohan <107089322+KengniJohan@users.noreply.github.com>
2023-04-01 13:23:55 +0100

    réalisation d'une partie de l'interface de visualisation des détails d'un besoin

--- android_part/app/src/main/java/com/android/openpressing/ui/theme/Color.kt
+++ android_part/app/src/main/java/com/android/openpressing/ui/theme/Color.kt
@@ -1,10 +1,14 @@
 package com.android.openpressing.ui.theme
 
 import androidx.compose.ui.graphics.Color
-
+val SoftPurple200 = Color(0xFFDEC7FA)
 val Purple200 = Color(0xFFBB86FC)
 val Purple500 = Color(0xFF6200EE)
 val Purple700 = Color(0xFF3700B3)
 val Teal200 = Color(0xFF03DAC5)
 
-val SurfaceColor = Color(0xABC0C5C4)
+val SurfaceColor = Color(0xABE8EDEC)
+
+val HardGray = Color(0xABA1A1A1)
+
+val SoftYellow = Color(0xFCD8D194)
