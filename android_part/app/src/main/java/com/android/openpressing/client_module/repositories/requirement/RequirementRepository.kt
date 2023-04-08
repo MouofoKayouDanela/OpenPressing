64d27fe8a575b39bc89d30ada7bb636aca870fa0
KengniJohan <107089322+KengniJohan@users.noreply.github.com>
2023-04-08 18:15:48 +0100

    Injection de l'interface RequirementApi

--- android_part/app/src/main/java/com/android/openpressing/client_module/repositories/requirement/RequirementRepository.kt
+++ android_part/app/src/main/java/com/android/openpressing/client_module/repositories/requirement/RequirementRepository.kt
@@ -3,8 +3,9 @@ package com.android.openpressing.client_module.repositories.requirement
 import com.android.openpressing.data.OpenPressingStrapiApi
 import javax.inject.Inject
 
-class RequirementRepository @Inject constructor(requirementApi: OpenPressingStrapiApi.RequirementApi) {
-
+class RequirementRepository @Inject constructor(
+    requirementApi: OpenPressingStrapiApi.RequirementApi
+) {
 
 
 }
