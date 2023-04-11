e3586b04b8ca00a4446a8c8ff5ca522051b46f7b
KengniJohan <107089322+KengniJohan@users.noreply.github.com>
2023-04-11 23:19:23 +0100

    Formatage d'un résultat de test suivant les models de données

--- android_part/app/src/main/java/com/android/openpressing/client_module/repositories/services/ServiceRepository.kt
+++ android_part/app/src/main/java/com/android/openpressing/client_module/repositories/services/ServiceRepository.kt
@@ -1,14 +1,15 @@
 package com.android.openpressing.client_module.repositories.services
 
 import com.android.openpressing.data.OpenPressingStrapiApi
-import com.android.openpressing.data.models.Service
+import com.android.openpressing.data.models.service.Service
+import com.android.openpressing.data.models.service.ServiceData
 import javax.inject.Inject
 
 class ServiceRepository @Inject constructor(
     private val serviceApi: OpenPressingStrapiApi.ServiceApi
     ) {
 
-    suspend fun getAll() : List<Service> = serviceApi.getAll()
+    suspend fun getAll() : MutableList<ServiceData> = serviceApi.getAll().data
 
     suspend fun getById(id: Int) : Service = serviceApi.getById(id)
 
