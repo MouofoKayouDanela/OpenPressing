07acaee972b7ee49a5342f54f4e49d92ddac0ce0
KengniJohan <107089322+KengniJohan@users.noreply.github.com>
2023-04-14 02:08:15 +0100

    Initialisation de la création de la methode save()

--- android_part/app/src/main/java/com/android/openpressing/client_module/viewmodels/services/ServiceViewModel.kt
+++ android_part/app/src/main/java/com/android/openpressing/client_module/viewmodels/services/ServiceViewModel.kt
@@ -4,6 +4,7 @@ import android.util.Log
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.android.openpressing.client_module.repositories.services.ServiceRepository
+import com.android.openpressing.data.models.service.Service
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
@@ -12,21 +13,55 @@ import javax.inject.Inject
 @HiltViewModel
 class ServiceViewModel @Inject constructor(
     private val serviceRepository: ServiceRepository
-    ) : ViewModel() {
+    ) : ViewModel()
+{
 
         fun getAll() {
 
             viewModelScope.launch(Dispatchers.IO) {
-                val services = serviceRepository.getAll()
-                Log.i("", "$services")
+                try {
+                    val services = serviceRepository.getAll()
+                } catch (e: Exception) {
+
+                }
             }
 
         }
 
         fun getById(id: Int) {
-            viewModelScope.launch(Dispatchers.IO) {
-                val service = serviceRepository.getById(id)
-                Log.i("", "$service")
+            try {
+                viewModelScope.launch(Dispatchers.IO) {
+                    val service = serviceRepository.getById(id)
+                }
+            } catch (e: Exception) {
+
+            }
+        }
+
+        fun save(service: Service) {
+            try {
+
+                viewModelScope.launch(Dispatchers.IO) {
+                    serviceRepository.save(service)
+                }
+
+            } catch (e: Exception) {
+
+            }
+        }
+
+        fun update(id: Int, service: Service) {
+            try {
+
+                viewModelScope.launch(Dispatchers.IO) {
+                    val updatedService = serviceRepository.update(id, service)
+                    if (updatedService.equals(service)){
+
+                    }
+                }
+
+            } catch (e: Exception) {
+
             }
         }
 }
