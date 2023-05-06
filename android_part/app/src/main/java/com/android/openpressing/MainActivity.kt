59b31bdc2df6a4e7708ff8a8e31fdb5f68d3cc70
unknown <bravezebaze@gmail.com>
2023-05-06 16:56:18 +0200

    insertion des interfaces connexion et inscription

--- android_part/app/src/main/java/com/android/openpressing/MainActivity.kt
+++ android_part/app/src/main/java/com/android/openpressing/MainActivity.kt
@@ -11,37 +11,42 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
 import com.android.openpressing.viewmodels.services.ServiceViewModel
 import com.android.openpressing.ui.theme.OpenPressingTheme
+import com.android.openpressing.client_module.presentation.module.FinitionScreen
+import com.android.openpressing.client_module.presentation.module.ForgotPasswordScreen
+import com.android.openpressing.client_module.presentation.module.LoginScreen
+import com.android.openpressing.client_module.presentation.module.RegisterScreen
+import com.android.openpressing.client_module.presentation.module.ResetPasswordScreen
 import dagger.hilt.android.AndroidEntryPoint
 
-@AndroidEntryPoint
+//@AndroidEntryPoint
+const val LOGIN_ROUTE = "/login"
+const val REGISTER_ROUTE = "/register"
+const val FINITION_ROUTE = "/finition"
+const val FORGOT_PASSWORD_ROUTE = "/forgot-password"
+const val RESET_PASSWORD_ROUTE = "/reset_password"
+
+
+
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            OpenPressingTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    Greeting("Android")
+            val navController = rememberNavController()
+
+            OpenPressingTheme{
+                NavHost(navController = navController, startDestination = LOGIN_ROUTE){
+                    composable(LOGIN_ROUTE){ LoginScreen(navController) }
+                    composable(REGISTER_ROUTE){ RegisterScreen(navController) }
+                    composable(FINITION_ROUTE) { FinitionScreen(navController)}
+                    composable(FORGOT_PASSWORD_ROUTE){ ForgotPasswordScreen(navController) }
+                    composable(RESET_PASSWORD_ROUTE){ ResetPasswordScreen(navController) }
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    OpenPressingTheme {
-        Greeting("Android")
     }
 }
