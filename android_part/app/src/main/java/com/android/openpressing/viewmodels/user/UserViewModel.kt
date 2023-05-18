76a94cf34c1ea49322d6f70d873e0553df2ce351
KengniJohan <107089322+KengniJohan@users.noreply.github.com>
2023-05-18 17:19:05 +0100

    Création du viewmodel et des états de user

--- android_part/app/src/main/java/com/android/openpressing/viewmodels/user/UserViewModel.kt
+++ android_part/app/src/main/java/com/android/openpressing/viewmodels/user/UserViewModel.kt
@@ -1,4 +1,43 @@
 package com.android.openpressing.viewmodels.user
 
-class UserViewModel {
+import android.view.WindowManager
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.android.openpressing.repositories.user.UserRepository
+import com.android.openpressing.viewmodels.services.state.UserState
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.launch
+import retrofit2.HttpException
+import javax.inject.Inject
+
+@HiltViewModel
+class UserViewModel @Inject constructor(
+    private val userRepository: UserRepository
+) : ViewModel() {
+
+    private val _userState = MutableStateFlow<UserState>(UserState.Empty)
+    val userState = _userState.asStateFlow()
+
+    fun getById(id: Int) {
+        _userState.value = UserState.Loading
+
+        try {
+            viewModelScope.launch(Dispatchers.IO) {
+
+                val user = userRepository.getById(id)
+                _userState.value = UserState.Success.UserSuccess(user)
+            }
+        } catch (exception: HttpException) {
+            _userState.value= UserState.Error("No internet connection")
+
+        }
+        catch (exception: WindowManager.InvalidDisplayException) {
+            _userState.value= UserState.Error("something went wong")
+
+        }
+    }
+
 }
