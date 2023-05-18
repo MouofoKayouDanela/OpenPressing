c9b2835c6ef48b10bc82859dd44aac06e6b3205f
KengniJohan <107089322+KengniJohan@users.noreply.github.com>
2023-05-18 17:14:12 +0100

    modification du type de la propriété profile_picture

--- android_part/app/src/main/java/com/android/openpressing/data/models/user/User.kt
+++ android_part/app/src/main/java/com/android/openpressing/data/models/user/User.kt
@@ -1,6 +1,6 @@
 package com.android.openpressing.data.models.user
 
-import com.android.openpressing.data.models.utils.images.ImageInfos
+import com.android.openpressing.data.models.utils.images.ImageInfo
 import com.google.gson.annotations.SerializedName
 import java.util.*
 
@@ -8,14 +8,14 @@ data class User (
     var id: Int? = null ,
     var name: String ,
     var surname: String ,
-    var birthday: Date,
+    var birthday: Date ,
     var gender: String ,
     @SerializedName("phoneNumber") var phone_number: String ,
-    @SerializedName("profilePicture") var profile_picture: ImageInfos ,
+    @SerializedName("profilePicture") var profile_picture: ImageInfo ,
     var username: String ,
     var email: String ,
     var password: String ,
     var registerDate: Date = Date() ,
-    var confirmed: Boolean = true,
+    var confirmed: Boolean = true ,
     var blocked: Boolean = false
 )
