86bfe4e185c786f3462451fcf88b6629019edbdf
KengniJohan <107089322+KengniJohan@users.noreply.github.com>
2023-05-21 23:09:58 +0100

    Ajout de l'attribut title

--- android_part/app/src/main/java/com/android/openpressing/data/models/laundry_type/LaundryTypeAttributes.kt
+++ android_part/app/src/main/java/com/android/openpressing/data/models/laundry_type/LaundryTypeAttributes.kt
@@ -1,8 +1,10 @@
 package com.android.openpressing.data.models.laundry_type
 
 import com.android.openpressing.data.models.laundry.Laundries
+import com.google.gson.annotations.SerializedName
 
 data class LaundryTypeAttributes(
+    @SerializedName("Title") var title: String ,
     var laundries: Laundries? = null ,
     var confirmed: Boolean = true,
     var blocked: Boolean = false ,
