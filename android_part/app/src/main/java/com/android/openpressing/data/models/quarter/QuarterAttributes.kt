31b6e720deac0113a64d45da09cb2fc389f95421
KengniJohan <107089322+KengniJohan@users.noreply.github.com>
2023-05-22 10:08:06 +0100

    Mise en relation du Quarter avec User

--- android_part/app/src/main/java/com/android/openpressing/data/models/quarter/QuarterAttributes.kt
+++ android_part/app/src/main/java/com/android/openpressing/data/models/quarter/QuarterAttributes.kt
@@ -4,11 +4,13 @@ import com.android.openpressing.data.models.agency.Agencies
 import com.android.openpressing.data.models.agency.Agency
 import com.android.openpressing.data.models.city.City
 import com.android.openpressing.data.models.order.Orders
+import com.android.openpressing.data.models.user.Users
 import com.google.gson.annotations.SerializedName
 
 data class QuarterAttributes(
     @SerializedName("Name") var name: String ,
     var city: City ,
+    var users: Users ,
     var agencies: Agencies? = null ,
     var orders: Orders? = null
 )
