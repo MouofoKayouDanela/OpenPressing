aa8f771481b713c99d2fdcc7947951dffc06a220
KengniJohan <107089322+KengniJohan@users.noreply.github.com>
2023-05-30 12:20:39 +0100

    Modification du style d'affichage des données

--- android_part/app/src/main/java/com/android/openpressing/client_module/presentation/besoin/ConsulterBesoin.kt
+++ android_part/app/src/main/java/com/android/openpressing/client_module/presentation/besoin/ConsulterBesoin.kt
@@ -1,55 +1,42 @@
 package com.android.openpressing.client_module.presentation.besoin
 
-import android.annotation.SuppressLint
-import android.app.Notification.MessagingStyle.Message
-import androidx.compose.foundation.ExperimentalFoundationApi
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
-import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
-import androidx.compose.material.MaterialTheme.colors
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.*
-import androidx.compose.material.icons.rounded.Delete
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.BlendMode.Companion.Screen
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.painter.Painter
-import androidx.compose.ui.graphics.vector.ImageVector
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
-import androidx.navigation.NavHostController
-import com.android.openpressing.R
-import com.android.openpressing.data.models.client.Client
+import com.android.openpressing.client_module.presentation.besoin.component.uil.MessagesLength
 import com.android.openpressing.data.models.client.ClientData
 import com.android.openpressing.data.models.requirement.RequirementData
 import com.android.openpressing.data.models.requirement_detail.RequirementDetailData
 import com.android.openpressing.ui.theme.Purple500
+import com.android.openpressing.ui.theme.primaryPrimeColor
+import com.android.openpressing.ui.theme.secondaryColor
 import com.android.openpressing.viewmodels.client.ClientViewModel
 import com.android.openpressing.viewmodels.requirement.RequirementViewModel
 import com.android.openpressing.viewmodels.requirement_detail.RequirementDetailViewModel
+import com.github.marlonlom.utilities.timeago.TimeAgo
 import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.flow.flowOn
-import java.util.Date
 
 @Composable
 fun MyNeed(
     rdViewModel: RequirementViewModel = hiltViewModel(),
     clientViewModel: ClientViewModel= hiltViewModel()
 ){
-    val userID = 4
+    val userID = 5
 
     val clients = remember(userID){ mutableStateOf<MutableList<ClientData>?>(null) }
     val client = remember(userID){ mutableStateOf<ClientData?>(null) }
@@ -57,11 +44,11 @@ fun MyNeed(
     val rdkey = "allrdkey"
     val requirements = remember(rdkey) { mutableStateOf<MutableList<RequirementData>?>(null) }
 
-    /*LaunchedEffect(key1 = userID){
-        clientViewModel.getAll()
+    LaunchedEffect(key1 = userID){
+        clientViewModel.findAll()
             .flowOn(Dispatchers.IO)
             .collect{ clients.value = it }
-    }*/
+    }
 
     if (clients.value != null){
 
@@ -98,9 +85,7 @@ fun MyNeed(
                     }
                 }
             )},
-        content= {
-                innerPadding->
-
+        content= { innerPadding ->
 
             if (client.value != null && requirements.value != null){
                 stock(
@@ -109,21 +94,11 @@ fun MyNeed(
                 )
             }
 
-
         },
         bottomBar= {BottomBar()}
-    )}
+    )
+}
 
-
-
-
-data class Contenu(
-    val Description: String,
-    val prix: String,
-    val date: String,
-)
-
-@OptIn(ExperimentalFoundationApi::class)
 @Composable
 fun stock(contenu: List<RequirementData>,
           innerPadding: PaddingValues){
@@ -134,6 +109,156 @@ fun stock(contenu: List<RequirementData>,
     }
 
 }
+
+@Composable
+fun Consult(
+    affiche: RequirementData ,
+    requiremenDetailsViewModel: RequirementDetailViewModel = hiltViewModel() ,
+) {
+
+    val requirementId= affiche.id!!
+
+    val allRd= remember(requirementId) {
+        mutableStateOf<List<RequirementDetailData>?>(null)
+    }
+
+    LaunchedEffect(key1 = allRd){
+        requiremenDetailsViewModel.getAll()
+            .flowOn(Dispatchers.IO)
+            .collect{
+                allRd.value = it.filter { oneOfAllRd ->
+                    affiche.attributes.requirement_details.data.any { wantedRd ->
+                        wantedRd.id == oneOfAllRd.id
+                    }
+                }
+            }
+    }
+
+    Card(
+            elevation = 3.dp,
+            shape = RoundedCornerShape(10.dp),
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(
+                        vertical = 8.dp ,
+                        horizontal = 16.dp
+                )
+    ) {
+        Column(modifier = Modifier.padding(16.dp)) {
+
+            Row(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(vertical = 8.dp) ,
+                    verticalAlignment = Alignment.CenterVertically
+            ){
+                var messagesLength = MessagesLength.NoMessages
+                Text(
+                        if (affiche.attributes.messages!!.data.isNullOrEmpty()) {
+                            "0 answer"
+                        } else {
+                            val messages = affiche.attributes.messages!!.data.size
+                            if (messages <= 1) {
+                                messagesLength = MessagesLength.BetweenOneAndFive
+                                "0$messages answer"
+                            } else {
+                                if (messages > 5) messagesLength = MessagesLength.MoreThanFive
+                                "$messages answers"
+                            }
+                        } ,
+                        color = Color.White ,
+                        modifier = Modifier
+                            .clip(CircleShape)
+                            .background(
+                                    when (messagesLength) {
+                                        MessagesLength.NoMessages -> Color.Red
+                                        MessagesLength.BetweenOneAndFive -> secondaryColor
+                                        else -> Color.Green
+                                    }
+                            )
+                            .padding(4.dp)
+
+                )
+            }
+
+            Column(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(vertical = 8.dp),
+                    verticalArrangement = Arrangement.Center
+            ){
+                if (allRd.value != null) {
+
+                    Row {
+                        Text(
+                                "Laundries",
+                                modifier = Modifier.weight(0.45f)
+                        )
+
+                        Text(
+                                ":",
+                                modifier = Modifier.weight(0.1f)
+                        )
+
+                        var quantity = 0
+                        allRd.value!!.forEach { quantity += it.attributes.quantity }
+                        Text(
+                                text = if (quantity < 10) "0$quantity"
+                                else quantity.toString() ,
+                                modifier = Modifier.weight(0.45f) ,
+                                fontWeight = FontWeight.SemiBold
+                        )
+                    }
+
+                    Row{
+                        Text(
+                                "price",
+                                modifier = Modifier.weight(0.45f)
+                        )
+
+                        Text(
+                                ":",
+                                modifier = Modifier.weight(0.1f)
+                        )
+
+                        var price = 0.0
+                        allRd.value!!.forEach { price += it.attributes.unitPrice * it.attributes.quantity }
+                        Text(
+                                text = "$price Fcfa" ,
+                                modifier = Modifier.weight(0.45f),
+                                fontWeight = FontWeight.SemiBold
+                        )
+                    }
+                }
+            }
+
+            Row(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(vertical = 8.dp) ,
+                    verticalAlignment = Alignment.CenterVertically
+            ) {
+                Text(
+                        text = TimeAgo.using(affiche.attributes.createdAt.time),
+                        modifier = Modifier.weight(0.9f)
+                )
+
+                IconButton(
+                        onClick = { /*TODO*/ },
+                        modifier = Modifier
+                            .clip(CircleShape)
+                            .background(primaryPrimeColor)
+                ) {
+                    Icon(
+                            Icons.Default.KeyboardArrowRight,
+                            contentDescription = null,
+                    )
+                }
+            }
+        }
+    }
+}
+
 @Composable
 fun BottomBar() {
     val selectedIndex = remember { mutableStateOf(0) }
@@ -178,163 +303,3 @@ fun BottomBar() {
             })
     }
 }
-
-@Composable
-fun Consult(
-    affiche: RequirementData,
-    requiremendetailsViewModel: RequirementDetailViewModel = hiltViewModel(),
-) {
-
-    val requirementId= affiche.id!!
-
-    val allRd= remember(requirementId) {
-        mutableStateOf<List<RequirementDetailData>?>(null)
-    }
-
-    LaunchedEffect(key1 = allRd){
-        requiremendetailsViewModel.getAll()
-            .flowOn(Dispatchers.IO)
-            .collect{ allRd.value = it }
-    }
-
-    Card(
-        elevation = 3.dp,
-        shape = RoundedCornerShape(10.dp),
-        modifier = Modifier
-            .fillMaxSize()
-            .padding(3.dp)
-    ) {
-        Row(modifier = Modifier.padding(8.dp)) {
-            Spacer(modifier = Modifier.width(16.dp))
-            Column(
-
-            ) {
-                if (allRd.value != null){
-
-
-                    var quantity by remember { mutableStateOf(0) }
-                    allRd.value!!.forEach{ quantity += it.attributes.quantity  }
-                    Text(
-                        text = "${ if(quantity<10) "0$quantity" else quantity.toString()} Vetement(s)",
-                        color = Color.Black,
-                        fontSize = 16.sp
-                    )
-                    Spacer(modifier = Modifier.height(height = 24.dp))
-
-                    var price by remember { mutableStateOf(0.0) }
-                    allRd.value!!.forEach{ price += it.attributes.unitPrice * it.attributes.quantity  }
-                    Text(
-                        text = "$price Fcfa",
-                        color = Color.Gray,
-                        fontSize = 16.sp
-                    )
-                }
-                Spacer(modifier = Modifier.height(height = 24.dp))
-                Text(
-                    text = affiche.attributes.createdAt.toString(),
-                    color = Color.Gray,
-                    fontSize = 16.sp
-                )
-
-            }
-            Spacer(modifier = Modifier.width(40.dp))
-            Column(
-                modifier = Modifier
-                    .padding(start = 16.dp)
-            ) {
-                Button(
-                    onClick = { /*TODO*/ },
-                    modifier = Modifier
-                        .width(200.dp)
-                        .padding(8.dp),
-                    colors = ButtonDefaults.buttonColors(backgroundColor = MaterialTheme.colors.primarySurface),
-                    shape = RoundedCornerShape(8.dp)
-                ) {
-                    Text("Consulter")
-                }
-            }
-        }
-    }
-}
-
-/*listOf(
-                        Contenu(
-                            Description = "5 Vetements",
-                            prix = "1500 FCFA",
-                            date = "publié le 22/03/2023"
-                        ),
-                        Contenu(
-                            Description = "10 vetements",
-                            prix = "1000 FCFA",
-                            date = "publié le 15/03/2023"
-
-                        ),
-                        Contenu(
-                            Description = "12 Vetements",
-                            prix = "3100 FCFA",
-                            date = "publié le 15/03/2023"
-
-                        ),
-                        Contenu(
-                            Description = "15 Vetements",
-                            prix = "4000",
-                            date = " publié le 15/05/2023"
-                        ),
-                        Contenu(
-                            Description = "20 Vetements",
-                            prix = "4000",
-                            date = " publié le 15/05/2023"
-                        ),
-                        Contenu(
-                            Description = "18 Vetements",
-                            prix = "4000",
-                            date = " publié le 15/05/2023"
-                        ),
-                        Contenu(
-                            Description = "15 Vetements",
-                            prix = "4000",
-                            date = " publié le 15/05/2023"
-                        ),
-                        Contenu(
-                            Description = "15 Vetements",
-                            prix = "4000",
-                            date = " publié le 15/05/2023"
-                        ),
-                        Contenu(
-                            Description = "15 Vetements",
-                            prix = "4000",
-                            date = " publié le 15/05/2023"
-                        ),
-                        Contenu(
-                            Description = "15 Vetements",
-                            prix = "4000",
-                            date = " publié le 15/05/2023"
-                        ),
-                        Contenu(
-                            Description = "15 Vetements",
-                            prix = "4000",
-                            date = " publié le 15/05/2023"
-
-                        ),
-                        Contenu(
-                            Description = "15 Vetements",
-                            prix = "4000",
-                            date = " publié le 15/05/2023"
-                        ),
-                        Contenu(
-                            Description = "15 Vetements",
-                            prix = "4000",
-                            date = " publié le 15/05/2023"
-                        ),
-                        Contenu(
-                            Description = "15 Vetements",
-                            prix = "4000",
-                            date = " publié le 15/05/2023"
-                        ),
-                        Contenu(
-                            Description = "15 Vetements",
-                            prix = "4000",
-                            date = " publié le 15/05/2023"
-                        )
-                    )*/
-
