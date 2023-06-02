2d98a5d1db2a9f116c55cb6cd37adfbcfaca6edf
KengniJohan <107089322+KengniJohan@users.noreply.github.com>
2023-06-02 14:49:11 +0100

    Finalisation des données statiques

--- android_part/app/src/main/java/com/android/openpressing/client_module/presentation/agence/ServicesNLaundriesManager.kt
+++ android_part/app/src/main/java/com/android/openpressing/client_module/presentation/agence/ServicesNLaundriesManager.kt
@@ -44,6 +44,7 @@ import com.android.openpressing.viewmodels.laundries.LaundryViewModel
 import com.android.openpressing.viewmodels.pressing.PressingViewModel
 import com.android.openpressing.viewmodels.services.ServiceViewModel
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.flow.flowOn
 
 
@@ -57,37 +58,8 @@ fun ServicesNLaundriesManager(
     serviceViewModel: ServiceViewModel = hiltViewModel()
 ) {
 
-    /*val agenceInfo = AgenceInfo(
-            "Elegance Pressing",
-            Icons.Rounded.LocalLaundryService,
-            "Bonamoussadi"
-    )
-    var laundries by remember {
-        mutableStateOf( listOf(
-            Laundry("Pantalon jean", Icons.Rounded.LocalLaundryService) ,
-            Laundry("Culotte jean", Icons.Rounded.LocalLaundryService) ,
-            Laundry("Robe en Nylon", Icons.Rounded.LocalLaundryService) ,
-            Laundry("Echarpe en coton", Icons.Rounded.LocalLaundryService) ,
-            Laundry("Pull en cuire", Icons.Rounded.LocalLaundryService) ,
-            Laundry("Cravate en soie", Icons.Rounded.LocalLaundryService) ,
-            Laundry("Veste en soie", Icons.Rounded.LocalLaundryService) ,
-            Laundry("Jacket jean", Icons.Rounded.LocalLaundryService) ,
-            Laundry("Jupe Nylon", Icons.Rounded.LocalLaundryService) ,
-            Laundry("Chemise coton", Icons.Rounded.LocalLaundryService)
-    )) }
-    var services by remember {
-        mutableStateOf(listOf(
-                Service("Nettoyage à sec" , Icons.Rounded.Wash) ,
-                Service("Nettoyage à eau" , Icons.Rounded.Wash) ,
-                Service("Lavage à sec" , Icons.Rounded.Wash) ,
-                Service("Déttachage" , Icons.Rounded.Wash) ,
-                Service("Blanchissage" , Icons.Rounded.Wash) ,
-                Service("Amidonnage" , Icons.Rounded.Wash)
-        ))
-    }*/
-
     var agencyLaundriesData by remember(agencyId) { mutableStateOf<List<IntermediaryData>?>(null) }
-    var fetchLaundriesData by remember { mutableStateOf(true) }
+    var fetchLaundriesData by remember { mutableStateOf(false) }
     if(fetchLaundriesData) {
         LaunchedEffect(key1 = agencyId) {
             var allALs: List<AgencyLaundryData>? = null
@@ -127,7 +99,7 @@ fun ServicesNLaundriesManager(
     }
 
     var agencyServicesData by remember(agencyId) { mutableStateOf<List<IntermediaryData>?>(null) }
-    var fetchServicesData by remember { mutableStateOf(true) }
+    var fetchServicesData by remember { mutableStateOf(false) }
     if(fetchServicesData) {
         LaunchedEffect(key1 = agencyId) {
             var allASs: List<AgencyServiceData>? = null
@@ -171,74 +143,18 @@ fun ServicesNLaundriesManager(
                  TopNavBar(navController)
             },
             content = { innerPadding ->
-                if(
-                    agencyLaundriesData != null &&
-                    agencyServicesData != null
-                ){
-                    BodyList(
-                            agencyId = agencyId ,
-                            innerPadding = innerPadding ,
-                            agencyLaundriesData = agencyLaundriesData!!,
-                            agencyServicesData = agencyServicesData!!,
-                            canFetchLaundriesData = { fetchLaundriesData = it },
-                            canFetchServicesData = { fetchServicesData = it }
-                    )
-                    fetchLaundriesData = false
-                    fetchServicesData = false
-                }
+                BodyList(
+                        agencyId = agencyId ,
+                        innerPadding = innerPadding ,
+                        agencyLaundriesData = agencyLaundriesData,
+                        agencyServicesData = agencyServicesData,
+                        canFetchLaundriesData = { fetchLaundriesData = it },
+                        canFetchServicesData = { fetchServicesData = it }
+                )
 
             },
     )
-                //BottomBar(navController)
 }
-
-
-
-/*fun BottomBar(navController: NavHostController) {
-    val selectedIndex = remember { mutableStateOf(0) }
-    BottomNavigation(
-        elevation = 2.dp,
-        backgroundColor = blanc
-    ) {
-
-        BottomNavigationItem(icon = {
-            Icon(imageVector = Icons.Default.LocalLaundryService, "", tint = Purple500)
-        },
-            label = { Text(text = "Laundry") },
-            selected = (selectedIndex.value == 0),
-            onClick = {
-                navController.navigate(Screen.Home.road)
-                selectedIndex.value = 0
-            })
-        BottomNavigationItem(icon = {
-            Icon(imageVector = Icons.Default.Reorder, "")
-        },
-            label = { Text(text = "Order") },
-            selected = (selectedIndex.value == 1),
-            onClick = {
-                selectedIndex.value = 1
-            })
-
-        BottomNavigationItem(icon = {
-            Icon(imageVector = Icons.Default.Chat, "")
-        },
-            label = { Text(text = "Chat") },
-            selected = (selectedIndex.value == 2),
-            onClick = {
-                selectedIndex.value = 2
-            })
-
-        BottomNavigationItem(icon = {
-            Icon(imageVector = Icons.Default.Person, "")
-        },
-            label = { Text(text = "Profile") },
-            selected = (selectedIndex.value == 3),
-            onClick = {
-                navController.navigate(Screen.Profile.road)
-                selectedIndex.value = 3
-            })
-    }
-}*/
 
 @Composable
 fun TopNavBar(navController: NavHostController) {
@@ -286,12 +202,20 @@ fun TopNavBar(navController: NavHostController) {
 fun BodyList(
     agencyId: Int,
     innerPadding: PaddingValues,
-    agencyLaundriesData: List<IntermediaryData>,
-    agencyServicesData: List<IntermediaryData>,
+    agencyLaundriesData: List<IntermediaryData>?,
+    agencyServicesData: List<IntermediaryData>?,
     canFetchLaundriesData: (Boolean) -> Unit,
     canFetchServicesData: (Boolean) -> Unit
 ) {
     var selectedIndex by remember { mutableStateOf(0) }
+
+    if(selectedIndex == 0) {
+        canFetchServicesData(true)
+        canFetchLaundriesData(false)
+    } else if (selectedIndex == 1) {
+        canFetchServicesData(false)
+        canFetchLaundriesData(true)
+    }
     
     LazyColumn(contentPadding = innerPadding) {
 
@@ -376,7 +300,13 @@ fun BodyList(
                     AddPrestationScreen(
                             selectedIndex = selectedIndex ,
                             updateDialogState = { showAddDialog = it } ,
-                            datas = if(selectedIndex == 0) agencyServicesData else agencyLaundriesData ,
+                            datas = if (selectedIndex == 0 && agencyServicesData != null)
+                                agencyServicesData
+                            else if (selectedIndex == 1 && agencyLaundriesData != null)
+                                agencyLaundriesData
+                            else
+                                listOf()
+                            ,
                             updateData = {
                                 addedData = it
                             }
@@ -399,10 +329,12 @@ fun BodyList(
         }
 
         items(
-                if(selectedIndex == 0)
+                if(selectedIndex == 0 && agencyServicesData != null)
                     agencyServicesData
-                else
+                else if (selectedIndex == 1 && agencyLaundriesData != null)
                     agencyLaundriesData
+                else
+                    listOf()
         ) { data ->
 
             Row(
@@ -437,18 +369,11 @@ fun BodyList(
                             )
                 )
 
+                var isDeleting by remember { mutableStateOf(false) }
+
                 IconButton(
                         onClick = {
-                            if (selectedIndex == 0) {
-//                                    val updatedServices = services.toMutableList()
-//                                    updatedServices.remove(Service(data.name , data.icon))
-//                                    updateServiceData(updatedServices.toList())
-                            } else {
-//                                    val updatedLaundries = laundries.toMutableList()
-//                                    updatedLaundries.remove(Laundry(data.name , data.icon))
-//                                    updateLaundryData(updatedLaundries.toList())
-                            }
-
+                            isDeleting = true
                         } ,
                         modifier = Modifier
                             .size(24.dp)
@@ -458,6 +383,16 @@ fun BodyList(
                             Icons.Rounded.Delete ,
                             contentDescription = null ,
                             tint = Purple200
+                    )
+                }
+                if(isDeleting) {
+                    DeleteData(
+                            agencyId = agencyId,
+                            selectedIndex = selectedIndex ,
+                            data = data ,
+                            canFetchLaundriesData = { canFetchLaundriesData(it) } ,
+                            canFetchServicesData = { canFetchServicesData(it) } ,
+                            updateDeletingState = { isDeleting = it }
                     )
                 }
             }
@@ -552,9 +487,9 @@ private fun SaveData(
     agencyId: Int,
     selectedIndex: Int,
     datas: List<IntermediaryData>,
+    updateData: (List<IntermediaryData>?) -> Unit,
     canFetchLaundriesData: (Boolean) -> Unit,
     canFetchServicesData: (Boolean) -> Unit,
-    updateData: (List<IntermediaryData>?) -> Unit,
     agencyViewModel: AgencyViewModel = hiltViewModel(),
     laundryAgencyViewModel: AgencyLaundryViewModel = hiltViewModel(),
     laundryViewModel: LaundryViewModel = hiltViewModel(),
@@ -563,8 +498,8 @@ private fun SaveData(
 ) {
     val agency = remember (agencyId) { mutableStateOf<Agency?>(null) }
     if(selectedIndex == 1) {
-        val laundry = remember(agencyId) { mutableStateOf<Laundry?>(null) }
-        LaunchedEffect(key1 = agencyId) {
+        val laundry = remember(selectedIndex) { mutableStateOf<Laundry?>(null) }
+        LaunchedEffect(key1 = selectedIndex) {
             agencyViewModel.findById(agencyId)
                 .flowOn(Dispatchers.IO)
                 .collect { agency.value = it }
@@ -584,11 +519,13 @@ private fun SaveData(
                         )
                     }
                 }
+                canFetchLaundriesData(true)
+                updateData(null)
             }
         }
     } else {
-        val service = remember(agencyId) { mutableStateOf<Service?>(null) }
-        LaunchedEffect(key1 = agencyId) {
+        val service = remember(selectedIndex) { mutableStateOf<Service?>(null) }
+        LaunchedEffect(key1 = selectedIndex) {
             agencyViewModel.findById(agencyId)
                 .flowOn(Dispatchers.IO)
                 .collect { agency.value = it }
@@ -608,16 +545,59 @@ private fun SaveData(
                         )
                     }
                 }
+                canFetchServicesData(true)
+                updateData(null)
             }
         }
     }
+}
 
+@Composable
+private fun DeleteData(
+    agencyId: Int,
+    selectedIndex: Int,
+    data: IntermediaryData,
+    canFetchLaundriesData: (Boolean) -> Unit,
+    canFetchServicesData: (Boolean) -> Unit,
+    updateDeletingState: (Boolean) -> Unit,
+    laundryAgencyViewModel: AgencyLaundryViewModel = hiltViewModel(),
+    serviceAgencyViewModel: AgencyServiceViewModel = hiltViewModel(),
+) {
 
-
-    if(selectedIndex == 0)
-        canFetchServicesData(true)
-    else
-        canFetchLaundriesData(true)
-    updateData(null)
+    LaunchedEffect(key1 = agencyId){
+        if (selectedIndex == 0) {
+            var agencyServices: List<AgencyServiceData>? = null
+            serviceAgencyViewModel.findAll()
+                .flowOn(Dispatchers.IO)
+                .collect { agencyServices = it }
+            if(agencyServices != null) {
+                val agencyService = agencyServices!!.find {
+                    agencyId == it.attributes.agency.data.id &&
+                            data.id == it.attributes.service.data.id
+                }
+                if(agencyService != null) {
+                    serviceAgencyViewModel.delete(agencyService.id!!)
+                    canFetchServicesData(true)
+                    updateDeletingState(false)
+                }
+            }
+        } else {
+            var agencyLaundries: List<AgencyLaundryData>? = null
+            laundryAgencyViewModel.findAll()
+                .flowOn(Dispatchers.IO)
+                .collect { agencyLaundries = it }
+            if(agencyLaundries != null) {
+                val agencyLaundry = agencyLaundries!!.find {
+                    agencyId == it.attributes.agency.data.id &&
+                            data.id == it.attributes.laundry.data.id
+                }
+                if(agencyLaundry != null) {
+                    laundryAgencyViewModel.delete(agencyLaundry.id!!)
+                    canFetchLaundriesData(true)
+                    updateDeletingState(false)
+                }
+            }
+        }
+    }
 }
 
