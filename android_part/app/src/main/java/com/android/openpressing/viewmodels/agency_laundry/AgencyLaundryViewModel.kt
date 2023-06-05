889451972b0990d955c5b40720f05faee3e8c6f3
KengniJohan <107089322+KengniJohan@users.noreply.github.com>
2023-06-05 09:45:37 +0100

    Modification de la methode save() et de la methode delete()

--- android_part/app/src/main/java/com/android/openpressing/viewmodels/agency_laundry/AgencyLaundryViewModel.kt
+++ android_part/app/src/main/java/com/android/openpressing/viewmodels/agency_laundry/AgencyLaundryViewModel.kt
@@ -6,11 +6,10 @@ import androidx.lifecycle.viewModelScope
 import com.android.openpressing.data.models.agency_laundry.AgencyLaundryData
 import com.android.openpressing.data.models.agency_laundry.AgencyLaundryInfo
 import com.android.openpressing.repositories.agency_laundry.AgencyLaundryRepository
+import com.android.openpressing.viewmodels.agency_laundry.state.AgencyLaundryState
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.flow
-import kotlinx.coroutines.flow.flowOn
+import kotlinx.coroutines.flow.*
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
@@ -19,19 +18,30 @@ class AgencyLaundryViewModel @Inject constructor(
     private val repository: AgencyLaundryRepository
 ) : ViewModel() {
 
+    private val _agencyLaundryState = MutableStateFlow<AgencyLaundryState>(AgencyLaundryState.Empty)
+    val agencyLaundryState = _agencyLaundryState.asStateFlow()
+
     fun findAll(): Flow<List<AgencyLaundryData>> = flow {
         emit(repository.getAll())
     }.flowOn(Dispatchers.IO)
 
     fun save(agencyLaundry: AgencyLaundryInfo) {
+        _agencyLaundryState.value = AgencyLaundryState.Loading
         viewModelScope.launch {
-            repository.save(agencyLaundry)
+            val response = repository.save(agencyLaundry)
+            _agencyLaundryState.value = AgencyLaundryState.Success.Save(response.isSuccessful)
         }
     }
 
-    fun delete(id: Int) {
-        viewModelScope.launch {
-            repository.delete(id)
-        }
-    }
+    fun delete(id: Int) = flow {
+        emit(repository.delete(id))
+    }.flowOn(Dispatchers.IO)
+
+//    fun delete(id: Int) {
+//        _agencyLaundryState.value = AgencyLaundryState.Loading
+//        viewModelScope.launch(Dispatchers.IO) {
+//            val response = repository.delete(id)
+//            _agencyLaundryState.value = AgencyLaundryState.Success.Delete(response.isSuccessful)
+//        }
+//    }
 }
