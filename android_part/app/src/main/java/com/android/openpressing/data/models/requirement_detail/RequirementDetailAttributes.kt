85cd69edfbcb1f3db251aba05e03ef848160487f
KengniJohan <107089322+KengniJohan@users.noreply.github.com>
2023-06-07 22:20:11 +0100

    Suppression des attributs name et description

--- android_part/app/src/main/java/com/android/openpressing/data/models/requirement_detail/RequirementDetailAttributes.kt
+++ android_part/app/src/main/java/com/android/openpressing/data/models/requirement_detail/RequirementDetailAttributes.kt
@@ -11,8 +11,6 @@ data class RequirementDetailAttributes(
     var service: Service ,
     @SerializedName("Quantity") var quantity : Int ,
     @SerializedName("UnitPrice") var unitPrice: Double ,
-    @SerializedName("Name") var name: String ,
-    @SerializedName("Description") var description: String ,
     val requirement: Requirement ,
     var createdAt: Date = Date() ,
     var updatedAt: Date = Date() ,
